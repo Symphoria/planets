77a3fb957ec108a83cd00c0f46f922483b2edc94
Harshit Jain <hj.harshit007@gmail.com>
2018-10-21 23:43:22 +0530

    Added Planet Card UI

--- lib/ui/home/Homepage.dart
+++ lib/ui/home/Homepage.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:planets/ui/home/GradientAppBar.dart';
+import 'package:planets/ui/home/HomePageBody.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Column(
-        children: <Widget>[GradientAppBar('treva')],
+        children: <Widget>[GradientAppBar('treva'), HomePageBody()],
       ),
     );
   }
