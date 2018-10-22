5f28cbbd2110dbcf60a001574ffc1b10b9a4b798
Harshit Jain <hj.harshit007@gmail.com>
2018-10-22 18:51:27 +0530

    Added scrollview to home page body

--- lib/ui/home/HomePageBody.dart
+++ lib/ui/home/HomePageBody.dart
@@ -5,6 +5,25 @@ import 'package:planets/model/Planet.dart';
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlanetRow(planets[4]);
+    return Expanded(
+      child: Container(
+        color: Color(0xFF736AB7),
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              sliver: SliverFixedExtentList(
+                itemExtent: 152.0,
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => PlanetRow(planets[index]),
+                  childCount: planets.length
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -24,7 +24,7 @@ class PlanetRow extends StatelessWidget {
         children: <Widget>[
           Image.asset(image, height: 12.0,),
           Container(width: 8.0),
-          Text(planet.gravity, style: MyTextStyle.regularTextStyle)
+          Text(value, style: MyTextStyle.regularTextStyle)
         ],
       );
     }
@@ -62,9 +62,7 @@ class PlanetRow extends StatelessWidget {
         color: Color(0xFF333366),
         shape: BoxShape.rectangle,
         borderRadius: BorderRadius.circular(8.0),
-        boxShadow: <BoxShadow>[
-          BoxShadow(color: Colors.black12, blurRadius: 10.0, offset: Offset(0.0, 10.0))
-        ]
+        boxShadow: <BoxShadow>[BoxShadow(color: Colors.black12, blurRadius: 10.0, offset: Offset(0.0, 10.0))]
       ),
       child: planetCardContent,
     );
